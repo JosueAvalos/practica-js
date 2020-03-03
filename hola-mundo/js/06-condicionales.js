328376ad357c91a892c8985158307509c9bc2e6d
JosueAvalos <josuebonito1993@gmail.com>
2020-03-02 22:37:15 -0600

    [MOD] condiciones.js line 47 of the 112

--- hola-mundo/js/06-condicionales.js
+++ hola-mundo/js/06-condicionales.js
@@ -40,8 +40,72 @@ var nombre = 'David Suarez';
 */
 
 if( edad >= 18 ){
-  console.log(nombre+" tiene " +edad+" años");
+  console.log(nombre+" tiene " +edad+" años, es mayor de edad");
+
+  if( edad <= 33 ){
+    console.log("Todavía eres millenial");
+  } else if( edad >= 70 ){
+    console.log("Eres anciano");    
+  } else {
+    console.log("Ya no eres millenial");  
+  }
+
+} else {
+  console.log(nombre+" tiene " +edad+" años, es menor de edad");
 }
-// } else {
-//   console.log("El usuario en menor de edad");
-// }
+
+edad = 50;
+nombre = 'David Cureño';
+
+console.log(nombre+" tiene " +edad+" años, es mayor de edad");
+if( edad <= 33 ){
+    console.log("Todavía eres millenial");
+  } else if( edad >= 70 ){
+    console.log("Eres anciano");    
+  } else {
+    console.log("Ya no eres millenial");  
+  }
+
+edad = 75;
+nombre = 'Luisa Garrido';
+
+console.log(nombre+" tiene " +edad+" años, es mayor de edad");
+if( edad <= 33 ){
+    console.log("Todavía eres millenial");
+  } else if( edad >= 70 ){
+    console.log("Eres anciano");    
+  } else {
+    console.log("Ya no eres millenial");  
+  }
+
+edad = 10;
+nombre = 'David Mendel'
+
+if( edad >= 18 ){
+  console.log(nombre+" tiene " +edad+" años, es mayor de edad");
+} else {
+  console.log(nombre+" tiene " +edad+" años, es menor de edad");
+}
+
+/*
+  Operadores Lógicos
+  AND(Y): &&
+  OR(O): ||
+  Negación: !
+*/
+
+var year = 2018;
+
+// Negación
+
+if(year != 2016){
+  console.log("El año no es 2016 realmente es: "+year);
+}
+
+// AND
+
+if(year >= 2000 && year <= 2020){
+  console.log("Estamos en la era actual"); 
+} else {
+  console.log("Estamos en la era post moderna");
+}
